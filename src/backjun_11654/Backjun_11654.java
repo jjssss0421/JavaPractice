a0f88757175398774dbbcf8dd6fb0cb16341ec56
Lee jeongsuk <Lee jeongsuk@DESKTOP-RJFT7AV>
2020-03-03 15:53:06 +0900

    2020. 03. 03 backjun_11720, 14719

---
+++ src/backjun_11654/Backjun_11654.java
@@ -0,0 +1,42 @@
+//========================
+// 2020년 02월 26일
+// 한수
+//========================
+// 자연수 num 입력.
+// 1보다 크거나 같고, num보다 작거나 같은 한수의 개수를 출력.
+//========================
+
+package backjun_11654;
+import java.util.*;
+
+public class Backjun_11654{
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		int num = input.nextInt();
+		
+		System.out.println(hanSu(num));
+	}
+	
+	static int hanSu(int num) {
+		int cnt=0;
+		for(int i=1; i<=num; i++) {
+			if(i<100) {
+				cnt++;
+			}
+			else {
+				String[] numArr = Integer.toString(i).split("");
+				Boolean flag = true;
+				for(int j=0; j<numArr.length-2; j++) {
+					if( Integer.parseInt(numArr[j+2]) - Integer.parseInt(numArr[j+1]) != Integer.parseInt(numArr[j+1])-Integer.parseInt(numArr[j]) ) {
+						flag=false;
+						break;
+					}
+				}
+				if(flag == true) {
+					cnt++;
+				}
+			}
+		}
+		return cnt;
+	}
+}
--- src/backjun_14719/Backjun_14719.java
+++ src/backjun_14719/Backjun_14719.java
@@ -1,56 +1,30 @@
 //========================
-// 2020년 03월 02일
-// 빗물
+// 2020년 03월 03일
+// 숫자의 합
 //========================
-// 2차원 세계에서는 한 칸의 용량은 1이다. 고이는 빗물의 총량을 출력하여라.
+// 첫째 줄에 숫자의 개수 N.
+// 둘째 줄에 숫자 N개가 공백없이.
+// 이 숫자를 모두 합해서 출력.
 //========================
 
 package backjun_14719;
 import java.util.*;
 
 public class Backjun_14719{
-	
 	public static void main(String args[]) {
 		Scanner input = new Scanner(System.in);
+		int N = input.nextInt();
+		String num = input.next();
+		String[] nums = num.split("");
 		
-		int[][] rain = new int[input.nextInt()][input.nextInt()];
-		
-		
-		for(int i=0; i<rain[0].length; i++) {
-			int height = input.nextInt();
-			for(int j=0; j<height; j++) {
-				rain[j][i]=1;
-			}
+		int sum=0;
+		for(int i=0; i<nums.length; i++) {
+			sum += Integer.parseInt(nums[i]);
 		}
 		
-		int cnt=0; 
-		int tmp=0;
-		for(int i=0; i<rain.length; i++) {
-			int j=0;
-			int k=rain[0].length-1;
-			for(;  j<rain[0].length; j++) {
-				if(rain[i][j]==1) break;
-			}
-			for(; k>=0; k--) {
-				if(rain[i][k]==1) break;
-			}
-			for(int l=j; l<=k; l++) {
-				if(rain[i][l]==0) cnt++;
-			}
-			System.out.println(i+" "+cnt);
-		}
-
-//		for(int i=0; i<rain.length; i++) {
-//			for(int j=0; j<rain[0].length; j++) {
-//				System.out.print(rain[i][j]+" ");
-//			}
-//			System.out.println();
-//		}
-		
-		
-		System.out.println(cnt);
+		System.out.println(sum);
+				
 	}
 	
-
-	
 }
+
